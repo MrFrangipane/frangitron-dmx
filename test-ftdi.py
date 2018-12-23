781aa6fe43ba78356d5425229e2456ed1b4e7a1a
MrFrangipane <mr@frangipane.me>
2018-12-23 14:55:13 +0100

    v0.7.0 : FTDI Tests OK

--- test-ftdi.py
+++ test-ftdi.py
@@ -1,12 +1,19 @@
-from pyftdi import ftdi
+"""
+Inspired by
+Copyright (C) Jonathan Brogdon <jlbrogdon@gmail.com>
+His program is published under a GPLv2 license
+"""
+import time
+import math
+from pyftdi.ftdi import Ftdi
 
 
-#FTDI device info
-vendor=0x0403
-product=0x6001
+VENDOR = 0x0403
+PRODUCT = 0x6001
+FRAMERATE = 90
 
 
-class OpenDmxUsb():
+class Interface(object):
     def __init__(self):
         self.baud_rate = 250000
         self.data_bits = 8
@@ -14,37 +21,49 @@ class OpenDmxUsb():
         self.parity = 'N'
         self.flow_ctrl = ''
         self.rts_state = 0
+        self._is_open = False
         self._init_dmx()
 
-    #Initialize the controller
     def _init_dmx(self):
-        self.ftdi=ftdi.Ftdi()
-        self.ftdi.open(vendor,product,0)
-        self.ftdi.set_baudrate(self.baud_rate)
-        self.ftdi.set_line_property(self.data_bits,self.stop_bits,self.parity,break_=0)
-        self.ftdi.set_flowctrl(self.flow_ctrl)
-        self.ftdi.purge_rx_buffer()
-        self.ftdi.purge_tx_buffer()
-        self.ftdi.set_rts(self.rts_state)
+        self.ftdi = Ftdi()
+        try:
+            self.ftdi.open(VENDOR, PRODUCT, 0)
+            self.ftdi.set_baudrate(self.baud_rate)
+            self.ftdi.set_line_property(self.data_bits, self.stop_bits, self.parity, break_=Ftdi.BREAK_OFF)
+            self.ftdi.set_flowctrl(self.flow_ctrl)
+            self.ftdi.purge_buffers()
+            self.ftdi.set_rts(self.rts_state)
+            self._is_open = True
+        except IOError, e:
+            print repr(e)
+            self._is_open = False
 
-    #Send DMX data
-    def send_dmx(self,channelVals):
-        self.ftdi.write_data(channelVals)
-        # Need to generate two bits for break
-        self.ftdi.set_line_property(self.data_bits,self.stop_bits,self.parity,break_=1)
-        self.ftdi.set_line_property(self.data_bits,self.stop_bits,self.parity,break_=1)
-        self.ftdi.set_line_property(self.data_bits,self.stop_bits,self.parity,break_=0)
+    def stream(self, universe_byte_array):
+        if not self._is_open: return
+
+        self.ftdi.write_data(universe_byte_array)
+        self.ftdi.set_line_property(self.data_bits, self.stop_bits, self.parity, break_=Ftdi.BREAK_ON)
+        self.ftdi.set_line_property(self.data_bits, self.stop_bits, self.parity, break_=Ftdi.BREAK_ON)
+
+        self.ftdi.close()
+        self._init_dmx()
+
+    def close(self):
+        if not self._is_open: return
+        self._is_open = False
+        self.ftdi.close()
 
 
+dmx = Interface()
+universe = bytearray([0] * 512)
+start_time = time.time()
 
 
-if __name__ == "__main__":
-    dmxUsb = OpenDmxUsb()
+while True:
+    elapsed = time.time() - start_time
 
-    channelVals = bytearray([0]*513)
-    channelVals[0] = 0 # dummy channel 0
+    universe[1] = int(255 * (0.5 * math.cos(elapsed * math.pi * 0.25) + 0.5))
+    universe[2] = int(255 * (0.5 * math.cos(elapsed * math.pi * 0.25 + math.pi) + 0.5))
 
-    while(True):
-        channelVals[1] = 255
-        channelVals[2] = 128
-        dmxUsb.send_dmx(channelVals)
+    dmx.stream(universe)
+    time.sleep(1 / float(FRAMERATE))
