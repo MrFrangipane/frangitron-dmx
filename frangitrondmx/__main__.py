9cbef100396d2ae3da7a925f46ce5c809b2966b7
mrfrangipane <mrfrangipane@frangi.pane>
2018-12-31 16:42:00 +0100

    v0.15.1 : Fix conditional import for PySide

--- frangitrondmx/__main__.py
+++ frangitrondmx/__main__.py
@@ -1,17 +1,17 @@
 import os
 import sys
 from streamer import Streamer
-from webapp import serve_webapp
-from editor import launch_editor
 
 
 if __name__ == '__main__':
     if len(sys.argv) == 3:
         if os.path.isdir(sys.argv[1]) and os.path.isfile(sys.argv[2]):
+            from webapp import serve_webapp
             fixtures_folder = sys.argv[1]
             programs_file = sys.argv[2]
             serve_webapp(Streamer(fixtures_folder, programs_file))
 
         if sys.argv[1] == "editor" and os.path.isdir(sys.argv[2]):
+            from editor import launch_editor
             fixtures_folder = sys.argv[2]
             launch_editor(fixtures_folder)
